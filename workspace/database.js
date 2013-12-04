c379559113c92b41a22f2eab50b6361221c3ce44
Gurr3 <gurr72@gmail.com>
2013-12-04 18:01:34 +0100

    updates

--- workspace/database.js
+++ workspace/database.js
@@ -1,5 +1,6 @@
 var mongoose = require("mongoose");
 var db = mongoose.createConnection("localhost", "soundslike");
+var crypt = require("./crypt");
 //var schemas = require("./initSchemas");
 //schemas.init();
 exports.getlvl0 = getlvl0;
@@ -8,22 +9,116 @@ exports.getlvl1 = getlvl1;
 exports.addlvl1 = addlvl1;
 exports.getlvl2 = getlvl2;
 exports.addlvl2 = addlvl2;
+exports.findOrCreate = findOrCreate;
+exports.getbyname= getbyname;
+exports.getany=getany;
 
-var lvl0Schema = new mongoose.Schema(
-		{name: String},
-		{info: [String]}
-	);
-var lvl1Schema = new mongoose.Schema(
-		{name: String},
-		{info: [String]}
-	);
-var lvl2Schema = new mongoose.Schema(
-		{name: String},
-		{info: [String]}
-	);
+var lvl0Schema = new mongoose.Schema({
+		name: String,
+		rating: Number,
+		//picture should end on .png/.jpg and is web address
+		picture: String,
+		soundslike: {},
+		info: {}
+	});
+var lvl1Schema = new mongoose.Schema({
+		name: String,
+		rating: Number,
+		picture: String,
+		soundslike: {},
+		parent: Number,
+		info: {}
+	});
+var lvl2Schema = new mongoose.Schema({
+		name: String,
+		rating: Number,
+		picture: String,
+		soundslike: {},
+		parent: Number,
+		info: {}
+	});
+var userSchema = new mongoose.Schema({
+	openID: String
+	});
 
 //lvl01Schema.methods.sayName = function(){
 //		console.log("This name: "+this.name);};
+
+
+
+//This Method finds openId and returns 
+//call with
+//Callback(err, res)
+function findOrCreate(openId, salt, callback){
+	var saltedid = crypt.hash(openId,salt);
+	db.once("open", function(){
+		var user = db.model("users",userSchema);
+		user.find({openID:saltedid}, function(err, res){
+			if (err){
+				//console.log(err);
+				callback(err, null);
+			}
+			else if (res[0]){
+				//console.log("in callback res "+res);
+				callback(err, res[0]._id);
+
+			} else {
+				//console.log("in make new user");
+				var newUser = new user({openID:saltedid});
+				newUser.save(function(errs){
+					if (errs) {
+						callback("could not make openID", null);
+					}
+					else {
+						console.log("made new user");
+						user.find({openID:saltedid}, function(erre, res){
+							callback(erre, res[0]._id);
+						})
+					}
+				});
+			}
+		});
+	})
+}
+//num = what lvl
+function getbyname(num, Name, callback){
+	if (num==0) {
+		var lvl0 = db.model("lvl0",lvl0Schema);
+		getbynameHelper(lvl0, Name,function(err, res){
+			callback(err,res);});
+	}
+	if (num==1) {
+		var lvl1 = db.model("lvl1",lvl1Schema);
+		getbynameHelper(lvl1, Name,function(err, res){
+			callback(err,res);})
+	}
+	if (num==2) {
+		var lvl2 = db.model("lvl2",lvl2Schema);
+		getbynameHelper(lvl2, Name,function(err, res){
+			callback(err,res);
+})}}
+function getbynameHelper(lvl, Name, callback){
+	db.once("open", function(){
+		lvl.find({name:Name}, function(err, res){
+			callback(err, res);
+})})}
+
+function getany(num, what, ID, callback){
+if (num==0) {
+		var lvl0 = db.model("lvl0",lvl0Schema);
+		getbyidHelper(lvl0, what, ID,function(err, res){
+			callback(err,res);
+		});
+	}
+}
+function getbyidHelper(lvl, what, Name, callback){
+	db.once("open", function(){
+		lvl.find({what:Name}, function(err, res){
+			callback(err, res);
+})})}
+
+
+
 
 
 
@@ -32,21 +127,19 @@ function getlvl0(artistName, callback){
 	db.once("open", function(){
 		var lvl0 = db.model("lvl0",lvl0Schema);
 		lvl0.find({name:artistName}, function(err, res){
-			callback(res);
+			callback(err, res);
 		})
 	});
 }
 
-function addlvl0(artistName, callback){
+function addlvl0(artistName, arg2, callback){
 	db.once("open", function(){
 		var lvl0 = db.model("lvl0",lvl0Schema);
-		var metallica = new lvl0({name:"Niklas"}, {info:["Niklas", "hej"]});
-		//Save entity in db
-		metallica.save(function(err){
-			//if (err){console.log()};
+		var newlvl0 = new lvl0({name:artistName, info:arg2 });
+		newlvl0.save(function(err){
+			if (err){callback(err);}
+			else{callback(null)}
 		});
-		console.log("in add lvl");
-		callback();
 	});
 }
 
@@ -54,25 +147,9 @@ function addlvl0(artistName, callback){
 
 function getlvl1(albumName, callback){
 	db.once("open", function(){
-		//Create a mongoose Schema
-		var albumSchema = new mongoose.Schema({
-			name: String});
-		//Add a method to this Schema, for fun :)
-		albumSchema.methods.sayName = function(){
-			console.log("This name: "+this.name);
-		};
-		var album = db.model("lvl1",albumSchema);
-		//var metallica = new artist({name:"Niklas"});
-		//Save entity in db
-		//metallica.save();
-		//Find all name:"Niklas" and for every item found, use the method declared earlier for printing
-		album.find({name:albumName}, function(err, res){
-			callback(res);
-
-			//example use of res
-			//for (var i = res.length - 1; i >= 0; i--) {
-			//	res[i].sayName();
-			//};	
+		var lvl1 = db.model("lvl1",albumSchema);
+		lvl1.find({name:albumName}, function(err, res){
+			callback(err, res);
 		})
 	});
 }
@@ -80,36 +157,19 @@ function addlvl1(albumName, callback){
 	db.once("open", function(){
 		var lvl1 = db.model("lvl1",lvl1Schema);
 		var metallica = new lvl1({name:"Niklas"}, {info:["Niklas", "hej"]});
-		//Save entity in db
 		metallica.save(function(err){
-			//if (err){console.log()};
+			if (err){callback(err);}
+			else{callback(null)}
 		});
-		console.log("in add lvl");
-		callback();
+		
 	});
 }
 
 function getlvl2(trackName, callback){
 	db.once("open", function(){
-		//Create a mongoose Schema
-		var trackSchema = new mongoose.Schema({
-			name: String});
-		//Add a method to this Schema, for fun :)
-		trackSchema.methods.sayName = function(){
-			console.log("This name: "+this.name);
-		};
-		var track = db.model("lvl2",trackSchema);
-		//var metallica = new artist({name:"Niklas"});
-		//Save entity in db
-		//metallica.save();
-		//Find all name:"Niklas" and for every item found, use the method declared earlier for printing
-		track.find({name:trackName}, function(err, res){
-			callback(res);
-
-			//example use of res
-			//for (var i = res.length - 1; i >= 0; i--) {
-			//	res[i].sayName();
-			//};	
+		var lvl2 = db.model("lvl2",trackSchema);
+		lvl2.find({name:trackName}, function(err, res){
+			callback(err, res);
 		})
 	});
 }
@@ -117,11 +177,10 @@ function addlvl2(trackName, callback){
 	db.once("open", function(){
 		var lvl2 = db.model("lvl2",lvl2Schema);
 		var metallica = new lvl2({name:"Niklas"}, {info:["Niklas", "hej"]});
-		//Save entity in db
 		metallica.save(function(err){
-			//if (err){console.log()};
+			if (err){callback(err);}
+			else{callback(null)}
 		});
-		console.log("in add lvl");
-		callback();
+		
 	});
 }
